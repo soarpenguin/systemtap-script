16c1ed482887e1ca9cf5092b88c1cdb8557e9dfe
soarpenguin <soarpenguin@gmail.com>
2013-01-02 04:23:09 +0800

    stpscript2ko.pl - add (-r DIR) for build specified kernel build tree.

--- stpscript2ko.pl
+++ stpscript2ko.pl
@@ -2,6 +2,9 @@
 #
 # use for to convert script to kernel module.
 #   stap -r 2.6.35.14-106.fc14.i686 nettop.stp -m nettop -p4
+#       
+#       -p NUM Stop  after  pass  NUM.   The passes are numbered 1-5: parse, elaborate, translate,
+#              compile, run.  See the PROCESSING section for details.
 
 use strict;
 use warnings;
@@ -13,23 +16,32 @@ use Term::ANSIColor;
 my $uname;
 chomp($uname = `uname -r`);
 ## $uname
-my $myscript;
-my $mymodule;
+my $myscript; # systemstp script to convert. 
+my $mymodule; # kernel module name.
+my $kerneldir; # kernel source code dir.
 my $program = &_my_program();
 
-my $command = 'stap -r ';
+my $command = 'stap -r';
 
 my $usage = "
-Usage: $program [option]...
+Usage: $program [option] ...
 
        -h, --help 
             Display this help and exit
 
-       -m, --module
-            The output module name(default: $program\.ko).
+       -m [modulename], --module [modulename]
+            The output module name(default: scriptname.ko).
 
-       -s, --script
+       -s scriptname, --script scriptname
             The script name want to converted to *.ko.
+       
+       -r /DIR
+              Build  for  kernel  in given build tree. Can also 
+              be set with the SYSTEMTAP_RELEASE environment variable.
+
+       -r RELEASE (default)
+              Build for kernel in build tree /lib/modules/RELEASE/build. 
+              Can also  be  set  with the SYSTEMTAP_RELEASE environment variable.
 
        -V   Display version information.
 ";
@@ -37,7 +49,8 @@ Usage: $program [option]...
 my $ret = GetOptions(
     'script|s=s' => \$myscript,
     'module|m=s' => \$mymodule,
-    'help|h'     => \&usage
+    'help|h'     => \&usage,
+    'r=s'        => \$kerneldir
 );
 
 $| =1;
@@ -64,10 +77,30 @@ unless($mymodule) {
 ## $mymodule
 
 ## the command to finish convertion
-if($uname =~ /^\'([^']+)\'/) {
-    $uname = $1;
+if(! $kerneldir) {
+    &myprint("\nUse current running kernel: \"$uname\"!\n");
+    if($uname =~ /^\'([^']+)\'/) {
+        $uname = $1;
+    }
+    # print "$uname\n";
+} else {
+    if(-e $kerneldir) {
+        ## $kerneldir
+        if($kerneldir =~ /((.)+)\/$/) {
+            $uname = $1;
+        } else {
+            $uname = $kerneldir;
+        }
+        ## $uname
+        &myprint("Use kernel build tree: \"$uname\"!\n");
+    } else {
+        &myprint("The kernel build tree: \"$kerneldir\" not exists!\n");
+        &myprint("Use current running kernel: \"$uname\"!\n");
+        if($uname =~ /^\'([^']+)\'/) {
+            $uname = $1;
+        }
+    }
 }
-# print "$uname\n";
 
 my $result = `$command $uname $myscript -m $mymodule -p4`;
 chomp($result);
@@ -95,5 +128,11 @@ sub mydie {
     print("@_ \n");
     print color("reset");
     &usage();
+}
+
+sub myprint {
+    print color("red");
+    print("@_ \n");
+    print color("reset");
 }
 
